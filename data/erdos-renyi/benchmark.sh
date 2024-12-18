b47c7ee3796e51950fc4d9dc9bf41d66d3271621
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-12-18 09:21:40 +0100

    #2: import data about automatically generated (small) graph using Erdos-Renyi scheme

---
+++ data/erdos-renyi/benchmark.sh
@@ -0,0 +1,10 @@
+
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false > 2k-0001p-02e.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 true >> 2k-0001p-02e.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false > 2k-0001p-01e.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 true >> 2k-0001p-01e.out
+
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false > 5k-0001p-02e.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 true >> 5k-0001p-02e.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false > 5k-0001p-01e.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 true >> 5k-0001p-01e.out
