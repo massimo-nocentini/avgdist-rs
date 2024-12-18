9ed6af551fe7764b6532d88611511805e115e974
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-12-18 10:18:22 +0100

    #2: Adjust importing for webgraph

--- data/erdos-renyi/webgraph-from.sh
+++ data/erdos-renyi/webgraph-from.sh
@@ -1,10 +1,10 @@
 
-webgraph from arcs --exact --num-nodes 842 1k-0001p < 1k-0.001p.csv
+webgraph from arcs --exact --num-nodes 1000 1k-0001p < 1k-0.001p.csv
 webgraph from arcs --exact --source-column 1 --target-column 0 --num-nodes 842 1k-0001p-t < 1k-0.001p.csv
 webgraph build ef 1k-0001p
 webgraph build ef 1k-0001p-t
 
-webgraph from arcs --exact --num-nodes 1964 2k-0001p < 2k-0.001p.csv
+webgraph from arcs --exact --num-nodes 2000 2k-0001p < 2k-0.001p.csv
 webgraph from arcs --exact --source-column 1 --target-column 0 --num-nodes 1964 2k-0001p-t < 2k-0.001p.csv
 webgraph build ef 2k-0001p
 webgraph build ef 2k-0001p-t
