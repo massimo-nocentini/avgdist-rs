423dcc2a792a4c125a1c7f707165a4566d7a7227
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-12-18 09:32:16 +0100

    #2: Update the benchmark script to stress 1k graph too.

--- data/erdos-renyi/benchmark.sh
+++ data/erdos-renyi/benchmark.sh
@@ -1,10 +1,44 @@
 
-time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false > 2k-0001p-02e.out
-time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 true >> 2k-0001p-02e.out
-time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false > 2k-0001p-01e.out
-time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 true >> 2k-0001p-01e.out
 
-time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false > 5k-0001p-02e.out
-time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 true >> 5k-0001p-02e.out
-time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false > 5k-0001p-01e.out
-time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 true >> 5k-0001p-01e.out
+mkdir -p result
+
+
+# 1k
+
+## epsilon = 0.2
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 false true > result/1k-0001p-02e-d.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 false false > result/1k-0001p-02e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 true false >> result/1k-0001p-02e.out
+
+
+## epsilon = 0.1
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 false true > result/1k-0001p-01e-d.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 false false > result/1k-0001p-01e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 true false >> result/1k-0001p-01e.out
+
+# 2k
+
+## epsilon = 0.2
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false true > result/2k-0001p-02e-d.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false false > result/2k-0001p-02e.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 true false >> result/2k-0001p-02e.out
+
+
+## epsilon = 0.1
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false true > result/2k-0001p-01e-d.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false false > result/2k-0001p-01e.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 true false >> result/2k-0001p-01e.out
+
+
+# 5k
+
+## epsilon = 0.2
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false true > result/5k-0001p-02e-d.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false false > result/5k-0001p-02e.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 true false >> result/5k-0001p-02e.out
+
+## epsilon = 0.1
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false true > result/5k-0001p-01e-d.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false false > result/5k-0001p-01e.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 true false >> result/5k-0001p-01e.out
+
