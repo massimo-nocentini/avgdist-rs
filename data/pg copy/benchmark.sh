7acf53304b3dd2ec39e7c42176189a37c63dae61
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-02 11:18:02 +0100

    Fix lunching the prog via cargo

--- data/pg copy/benchmark.sh
+++ data/pg copy/benchmark.sh
@@ -3,6 +3,6 @@
 mkdir -p result
 
 
-time avgdist-rs /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false false > result/atg.out
-time avgdist-rs /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false true > result/atg-d.out
+time cargo run --release -- /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false false > result/atg.out
+time cargo run --release -- /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false true > result/atg-d.out
 
