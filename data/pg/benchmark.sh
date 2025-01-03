82c382e6ee3f6aa98f5ab5774313122b8a3841e9
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-03 09:30:19 +0100

    #5 fix the benchmark script

--- data/pg/benchmark.sh
+++ data/pg/benchmark.sh
@@ -3,6 +3,6 @@
 mkdir -p result
 
 
-time cargo run --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false true > result/pg-d.out
-time cargo run --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false false > result/pg.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false true > result/pg-d.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false false > result/pg.out
 # don't do exact computation
