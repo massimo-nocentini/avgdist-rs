e5ed71fa0eb3824b065f416544ce69b07681ef43
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-03 09:32:23 +0100

    #5 forgot atg

--- data/atg/benchmark.sh
+++ data/atg/benchmark.sh
@@ -3,6 +3,6 @@
 mkdir -p result
 
 
-time cargo run --release -- /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false false > result/atg.out
-time cargo run --release -- /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false true > result/atg-d.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false false > result/atg.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/address-transaction-graph/atg /data/bitcoin/address-transaction-graph/atg-t 61 0.1 false true > result/atg-d.out
 
