edb73da89bba48f9d20b72430dfbb6528f25c523
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-04 15:32:53 +0100

    #5 update results about toy graphs

--- data/pg/benchmark.sh
+++ data/pg/benchmark.sh
@@ -3,6 +3,6 @@
 mkdir -p result
 
 
-time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false true > result/pg-d.out
 time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false false > result/pg.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false true > result/pg-d.out
 # don't do exact computation
