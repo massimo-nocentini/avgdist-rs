4ee3cd37a329d392f7dc5f4f5e6d9e24f2d3b860
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-07 11:24:19 +0100

    Issue #5: results about the payment graph.

--- data/pg/benchmark.sh
+++ data/pg/benchmark.sh
@@ -3,6 +3,6 @@
 mkdir -p result
 
 
-time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false false > result/pg.out
-time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 61 0.1 false true > result/pg-d.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 222 0.1 false false > result/pg-222.out
+time cargo run --bin avgdist-rs --release -- /data/bitcoin/bitcoin-webgraph/pg /data/bitcoin/bitcoin-webgraph/pg-t 222 0.1 false true > result/pg-d-222.out
 # don't do exact computation
