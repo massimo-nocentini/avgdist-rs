222ced003d987d6c68387a5419d5a60c0a826d7e
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-08 12:00:16 +0100

    Issue #1 attempt to use the parallel BFS.

--- src/bin/visit.rs
+++ src/bin/visit.rs
@@ -1,23 +1,19 @@
 use dsi_progress_logger::no_logging;
+use rand::random;
+use std::env;
 use std::ops::ControlFlow::Continue;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
-use webgraph::graphs::vec_graph::VecGraph;
-use webgraph::labels::proj::Left;
 use webgraph::prelude::BvGraph;
+use webgraph::traits::RandomAccessGraph;
 use webgraph::traits::SequentialLabeling;
-use webgraph_algo::algo::visits::breadth_first::{self, *};
 use webgraph_algo::algo::visits::Parallel;
-use webgraph_algo::{
-    prelude::breadth_first::{EventNoPred, ParFairNoPred},
-    threads,
-};
+use webgraph_algo::{prelude::breadth_first::EventNoPred, threads};
 
-fn main() {
-    let graph_filename = "";
-
-    let graph = BvGraph::with_basename(graph_filename).load().unwrap();
-
+fn visit<G>(root: usize, graph: G)
+where
+    G: RandomAccessGraph + Send + Sync,
+{
     let mut d = Vec::with_capacity(graph.num_nodes());
 
     for _ in 0..graph.num_nodes() {
@@ -25,19 +21,32 @@ fn main() {
     }
 
     let mut visit = webgraph_algo::algo::visits::breadth_first::ParFairNoPred::new(graph, 1);
+
     visit
         .par_visit(
-            0,
+            root,
             |event| {
                 // Set distance from 0
                 if let EventNoPred::Unknown { curr, distance, .. } = event {
                     d[curr].store(distance, Ordering::Relaxed);
                 }
-                Continue(())
+
+                Continue::<G>(())
             },
             &threads![],
             no_logging![],
         )
         .continue_value();
 }
- 
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let graph_filename = &args[1];
+
+    let graph = BvGraph::with_basename(graph_filename).load().unwrap();
+
+    let root = (random::<f64>() * (graph.num_nodes() as f64)).floor() as usize;
+
+    visit(root, graph);
+}
