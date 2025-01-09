0dd1096269fddb9b16cf3052a7d5a17ffaba80c9
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-09 20:41:06 +0100

    #6 fix for Erdos-Renyi graphs

--- data/erdos-renyi/benchmark.sh
+++ data/erdos-renyi/benchmark.sh
@@ -6,39 +6,39 @@ mkdir -p result
 # 1k
 
 ## epsilon = 0.2
-time cargo run --release --bin avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 false true > result/1k-0001p-02e-d.out
-time cargo run --release --bin avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 false false > result/1k-0001p-02e.out
-time cargo run --release --bin avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 true false >> result/1k-0001p-02e.out
+time cargo run --release --bin avgdist-rs 1k-0001p 1k-0001p-t 60 0.2 false true > result/1k-0001p-02e-d.out
+time cargo run --release --bin avgdist-rs 1k-0001p 1k-0001p-t 60 0.2 false false > result/1k-0001p-02e.out
+time cargo run --release --bin avgdist-rs 1k-0001p 1k-0001p-t 60 0.2 true false >> result/1k-0001p-02e.out
 
 
 ## epsilon = 0.1
-time cargo run --release --bin avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 false true > result/1k-0001p-01e-d.out
-time cargo run --release --bin avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 false false > result/1k-0001p-01e.out
-time cargo run --release --bin avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 true false >> result/1k-0001p-01e.out
+time cargo run --release --bin avgdist-rs 1k-0001p 1k-0001p-t 60 0.1 false true > result/1k-0001p-01e-d.out
+time cargo run --release --bin avgdist-rs 1k-0001p 1k-0001p-t 60 0.1 false false > result/1k-0001p-01e.out
+time cargo run --release --bin avgdist-rs 1k-0001p 1k-0001p-t 60 0.1 true false >> result/1k-0001p-01e.out
 
 # 2k
 
 ## epsilon = 0.2
-time cargo run --release --bin avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false true > result/2k-0001p-02e-d.out
-time cargo run --release --bin avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false false > result/2k-0001p-02e.out
-time cargo run --release --bin avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 true false >> result/2k-0001p-02e.out
+time cargo run --release --bin avgdist-rs 2k-0001p 2k-0001p-t 60 0.2 false true > result/2k-0001p-02e-d.out
+time cargo run --release --bin avgdist-rs 2k-0001p 2k-0001p-t 60 0.2 false false > result/2k-0001p-02e.out
+time cargo run --release --bin avgdist-rs 2k-0001p 2k-0001p-t 60 0.2 true false >> result/2k-0001p-02e.out
 
 
 ## epsilon = 0.1
-time cargo run --release --bin avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false true > result/2k-0001p-01e-d.out
-time cargo run --release --bin avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false false > result/2k-0001p-01e.out
-time cargo run --release --bin avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 true false >> result/2k-0001p-01e.out
+time cargo run --release --bin avgdist-rs 2k-0001p 2k-0001p-t 60 0.1 false true > result/2k-0001p-01e-d.out
+time cargo run --release --bin avgdist-rs 2k-0001p 2k-0001p-t 60 0.1 false false > result/2k-0001p-01e.out
+time cargo run --release --bin avgdist-rs 2k-0001p 2k-0001p-t 60 0.1 true false >> result/2k-0001p-01e.out
 
 
 # 5k
 
 ## epsilon = 0.2
-time cargo run --release --bin avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false true > result/5k-0001p-02e-d.out
-time cargo run --release --bin avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false false > result/5k-0001p-02e.out
-time cargo run --release --bin avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 true false >> result/5k-0001p-02e.out
+time cargo run --release --bin avgdist-rs 5k-0001p 5k-0001p-t 60 0.2 false true > result/5k-0001p-02e-d.out
+time cargo run --release --bin avgdist-rs 5k-0001p 5k-0001p-t 60 0.2 false false > result/5k-0001p-02e.out
+time cargo run --release --bin avgdist-rs 5k-0001p 5k-0001p-t 60 0.2 true false >> result/5k-0001p-02e.out
 
 ## epsilon = 0.1
-time cargo run --release --bin avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false true > result/5k-0001p-01e-d.out
-time cargo run --release --bin avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false false > result/5k-0001p-01e.out
-time cargo run --release --bin avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 true false >> result/5k-0001p-01e.out
+time cargo run --release --bin avgdist-rs 5k-0001p 5k-0001p-t 60 0.1 false true > result/5k-0001p-01e-d.out
+time cargo run --release --bin avgdist-rs 5k-0001p 5k-0001p-t 60 0.1 false false > result/5k-0001p-01e.out
+time cargo run --release --bin avgdist-rs 5k-0001p 5k-0001p-t 60 0.1 true false >> result/5k-0001p-01e.out
 
--- data/erdos-renyi/webgraph-from.sh
+++ data/erdos-renyi/webgraph-from.sh
@@ -1,16 +1,16 @@
 
 webgraph from arcs --exact --num-nodes 1000 1k-0001p < 1k-0.001p.csv
-webgraph from arcs --exact --source-column 1 --target-column 0 --num-nodes 842 1k-0001p-t < 1k-0.001p.csv
+webgraph transform transpose 1k-0001p 1k-0001p-t
 webgraph build ef 1k-0001p
 webgraph build ef 1k-0001p-t
 
 webgraph from arcs --exact --num-nodes 2000 2k-0001p < 2k-0.001p.csv
-webgraph from arcs --exact --source-column 1 --target-column 0 --num-nodes 1964 2k-0001p-t < 2k-0.001p.csv
+webgraph transform transpose 2k-0001p 2k-0001p-t
 webgraph build ef 2k-0001p
 webgraph build ef 2k-0001p-t
 
 webgraph from arcs --exact --num-nodes 5000 5k-0001p < 5k-0.001p.csv
-webgraph from arcs --exact --source-column 1 --target-column 0 --num-nodes 5000 5k-0001p-t < 5k-0.001p.csv
+webgraph transform transpose 5k-0001p 5k-0001p-t
 webgraph build ef 5k-0001p
 webgraph build ef 5k-0001p-t
 
