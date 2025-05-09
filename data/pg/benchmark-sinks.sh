bb76a832dbeaab60cd65bc9138805638dd36de9a
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-05-09 18:43:20 +0200

    New sinks results

--- data/pg/benchmark-sinks.sh
+++ data/pg/benchmark-sinks.sh
@@ -2,5 +2,5 @@
 
 mkdir -p result
 
-
-time cargo run --bin sinks --release -- /data/bitcoin/bitcoin-webgraph/pg 222 0.1  > result/sinks.out
+time cargo run --bin sink --release -- /data/bitcoin/bitcoin-webgraph/pg > result/sinks.out
+time cargo run --bin sink --release -- /data/bitcoin/bitcoin-webgraph/pg-t >> result/sinks.out
