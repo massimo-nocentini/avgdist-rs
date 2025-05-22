a508750a52b2b0130485d997d445462a5559e3cc
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-05-22 18:29:50 +0200

    New result for the Payment Graph with k - 346

--- data/pg/benchmark-unipair.sh
+++ data/pg/benchmark-unipair.sh
@@ -2,6 +2,6 @@
 
 mkdir -p result
 
-time cargo run --bin unipairs --release -- /data/bitcoin/bitcoin-webgraph/pg 62 0.1 false > result/pg-uni-222.out
-time cargo run --bin unipairs --release -- /data/bitcoin/bitcoin-webgraph/pg-t 62 0.1 true > result/pg-uni-222-closeness.out
+time cargo run --bin unipairs --release -- /data/bitcoin/bitcoin-webgraph/pg 222 0.1  > result/pg-uni-222.out
+#time cargo run --bin closeness --release -- /data/bitcoin/bitcoin-webgraph/pg-t 62 0.1 true > result/pg-uni-222-closeness.out
 
