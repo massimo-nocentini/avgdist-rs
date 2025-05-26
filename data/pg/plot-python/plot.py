a996fd1f7d09e87fe894e53ef62fb4ae33d72a75
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-05-26 13:13:31 +0200

    Fix the Dockerfile for plotting

--- data/pg/plot-python/plot.py
+++ data/pg/plot-python/plot.py
@@ -8,9 +8,13 @@ def plot_clustering(df: pl.DataFrame, output_file: str, plot_title: str):
     """
     Plot the distribution of the clustering coefficient.
     """
-    data = df['clustering']
+    #data = df['cc']
+    #collected = df.group_by("cc").agg(pl.len().alias("count")).collect()
+    collected = df.with_columns(pl.col("cc")).collect()
+    data = collected["cc"]
     plt.figure(figsize=(5, 5))
-    plt.hist(data, bins=np.arange(0, 1.1, 0.1), color="blue")
+    #plt.hist(data, bins=np.arange(0, 1.1, 0.1), color="blue")
+    plt.hist(data, color="blue")
     plt.title(plot_title)
     plt.xlabel("Closeness Centrality")
     plt.ylabel("Frequency")
@@ -18,7 +22,7 @@ def plot_clustering(df: pl.DataFrame, output_file: str, plot_title: str):
     plt.grid(linestyle='--', linewidth=0.5)
     plt.savefig(output_file, format='pdf', bbox_inches='tight')
     plt.close()
-    print(data.describe())
+    #print(df.describe())
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -27,5 +31,5 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     model_name = sys.argv[3]
-    df = pl.read_csv(input_file, separator=",", null_values=["nan", "-nan"])
+    df = pl.scan_csv(input_file, separator=",", null_values=["nan", "-nan"], schema=dict([("node", pl.Int64),("cc", pl.Float64)]))
     plot_clustering(df, output_file, f"{model_name} Closeness Centrality")
