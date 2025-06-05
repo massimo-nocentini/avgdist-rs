c8bad699af5d5ca256686045e36eb590fe4bc50f
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-06-05 07:25:38 +0200

    Results for ag

--- data/atg/benchmark-unipair.sh
+++ data/atg/benchmark-unipair.sh
@@ -3,4 +3,4 @@
 mkdir -p result
 
 time cargo run --bin unipairs --release -- /data/bitcoin/atg/atg 112 0.1 false > result/avgdist-uni.out
-time cargo run --bin harmonic --release -- /data/bitcoin/atg/atg-t 62 0.1 false > result/harmonic.out
+time cargo run --bin harmonic --release -- /data/bitcoin/atg/atg-t 32 0.1 false > result/harmonic.out
