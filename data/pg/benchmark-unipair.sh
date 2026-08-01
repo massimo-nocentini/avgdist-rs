08cc133f002b231926c2dfeed1a1862f9dcc5dc4
Massimo Nocentini <massimo.nocentini@gmail.com>
2026-08-01 15:55:12 +0200

    Refine a benchmark file

--- data/pg/benchmark-unipair.sh
+++ data/pg/benchmark-unipair.sh
@@ -2,6 +2,8 @@
 
 mkdir -p result
 
-time cargo run --bin unipairs --release -- /data/bitcoin/bitcoin-webgraph/pg 112 0.1 false > result/avgdist-uni.out
-time cargo run --bin harmonic --release -- /data/bitcoin/bitcoin-webgraph/pg-t 32 0.1 false > result/harmonic.out
+#time cargo run --bin unipairs --release -- /data/bitcoin/2022/tg/tg 112 0.1 false > /data/bitcoin/2022/sampling/tg/avgdist-uni-2022-tg.out
+#time cargo run --bin unipairs --release -- /data/bitcoin/2022/atg/atg 112 0.1 false > /data/bitcoin/2022/sampling/atg/avgdist-uni-2022-atg.out
+time cargo run --bin harmonic --release -- /data/bitcoin/2022/tg/tg-t 8 0.1 false > /data/bitcoin/2022/sampling/tg/harmonic-2022-tg.out
+#time cargo run --bin harmonic --release -- /data/bitcoin/2022/atg/atg-t 8 0.1 false > /data/bitcoin/2022/sampling/atg/harmonic-2022-atg.out
 
